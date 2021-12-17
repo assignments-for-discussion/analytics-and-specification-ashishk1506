4c561eaf01decfb89a04fdaa22c77a58e58c7042
ashishk1506 <2019ugec002@nitjsr.ac.in>
2021-12-17 18:27:15 +0530

    Test Passes

--- average.js
+++ average.js
@@ -1,8 +1,8 @@
 
 function average(numbers) {
   
-  let new_numbers = numbers.filter((data)=> isNaN(data) === false)
-  return new_numbers.reduce((p, c)=> p + c, 0) / z.length;
+  let newNumbers = numbers.filter((data)=> isNaN(data) === false)
+  return newNumbers.reduce((p, c)=> p + c, 0) / newNumbers.length;
 }
 
 module.exports = {average};
